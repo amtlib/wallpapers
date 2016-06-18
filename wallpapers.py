8c94bdba61909db1ca1001521fcfc71733c86ffb
maszeczuszets <maszeczuszets@protonmail.com>
2016-06-18 19:48:35 +0200

    Added useful comments

--- wallpapers.py
+++ wallpapers.py
@@ -1,10 +1,16 @@
-import re, praw, requests, os, glob, sys, bs4
+#Grzegorz 'maszeczuszets' Pach
+#Script takes name of subreddit, looks for imgur link, downloads image and sets wallpaper(Ubuntu only)
+
+
+import praw, requests, os, sys, bs4
 
 
 #CONFIG
 USER_AGENT = 'SOMERANDOMSHITSSS' 
 WALLPAPER_NAME = 'walp'
 
+
+#gets arguments from user - name of subreddit(source of images)
 if len(sys.argv) < 2:
 	print('Usage: ')
 	print('python {} subreddit'.format(sys.argv[0]))
@@ -14,7 +20,7 @@ elif len(sys.argv) >= 2:
 
 reddit = praw.Reddit(user_agent=USER_AGENT)
 
-
+#looking for random imgur link - if there is no imgur link in submission script is looking for diffrent one
 while True:
 	random_submission = reddit.get_subreddit(subreddit).get_random_submission()
 	if 'http://i.imgur.com/' in random_submission.url:
@@ -26,7 +32,7 @@ while True:
 		image_url = 'http:' + soup.findAll('img', {'class': 'post-image-placeholder'})[0]['src']
 		break
 
-
+#writing random image as file
 f = open(WALLPAPER_NAME, 'wb')
 r = requests.get(image_url)
 for chunk in r:
@@ -34,4 +40,5 @@ for chunk in r:
 
 f.close()
 
+#sets wallpaper - tested on ubuntu 16.04 LTS
 os.system('gsettings set org.gnome.desktop.background draw-background false && gsettings set org.gnome.desktop.background picture-uri file://{}/{}'.format(os.path.dirname(os.path.abspath(__file__)), WALLPAPER_NAME))
